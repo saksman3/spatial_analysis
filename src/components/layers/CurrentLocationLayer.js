ce4d326b57038070ac913c1e8db22173fc5c657c
rwittels <rwittels@sataxi.co.za>
2022-03-10 11:43:12 +0200

    Added code for map data point colours

--- src/components/layers/CurrentLocationLayer.js
+++ src/components/layers/CurrentLocationLayer.js
@@ -6,6 +6,14 @@ import htmlForFeature from 'utils/htmlForFeature';
 
 export const CURRENT_LOCATION_LAYER_ID = 'currentLocationLayer';
 
+const COLORSRGB = [
+  [255, 0, 0],
+  [0, 255, 0],
+  [0, 0, 255],
+  [255, 0, 255],
+  [255, 255, 0],
+];
+
 export default function CurrentLocationLayer() {
   const { currentLocationLayer } = useSelector((state) => state.carto.layers);
   const source = useSelector((state) => selectSourceById(state, currentLocationLayer?.source));
@@ -15,10 +23,24 @@ export default function CurrentLocationLayer() {
     return new CartoLayer({
       ...cartoLayerProps,
       id: CURRENT_LOCATION_LAYER_ID,
-      getFillColor: [241, 109, 122],
-      pointRadiusMinPixels: 2,
+      //getFillColor: [241, 109, 122],
+      getFillColor: (object) => {
+        if (object.properties.VehicleType !== 'Open Market Insurance' && object.properties.Manufacture == 'Toyota') {
+          return COLORSRGB[0];
+        } else if (object.properties.VehicleType !== 'Open Market Insurance' && object.properties.Manufacture == 'Nissan') {
+          return COLORSRGB[1];
+        } else if (object.properties.VehicleType !== 'Open Market Insurance' && object.properties.Manufacture == 'Mercedes') {
+          return COLORSRGB[2];
+        } else if (object.properties.VehicleType == 'Open Market Insurance') {
+          return COLORSRGB[3];
+        } else {
+          return COLORSRGB[4];
+        }
+      },
+      pointRadiusMinPixels: 3,
       getLineColor: [255, 0, 0],
-      lineWidthMinPixels: 1,
+      lineWidthMinPixels: 0,
+      lineWidthMaxPixels: 0,
       pickable: true,
       onHover: (info) => {
         if (info?.object) {
